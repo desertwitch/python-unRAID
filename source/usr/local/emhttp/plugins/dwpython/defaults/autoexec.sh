b2a8593a3d704435e7c33fbcb4962eff7a52574d
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-09-17 12:16:35 +0200

    Update autoexec.sh

--- source/usr/local/emhttp/plugins/dwpython/defaults/autoexec.sh
+++ source/usr/local/emhttp/plugins/dwpython/defaults/autoexec.sh
@@ -4,6 +4,6 @@
 # They are executed NON-BLOCKING (but sequentially), ensuring boot/(re-)install proceed regardless of any script errors.
 # The purpose of this script, for example, is to establish that certain packages are always installed by means of `pip`.
 #
-# All output is directed to the SYSLOG, to prevent this append '&>/dev/null' (without quotes) to the end of any command.
-# or, alternatively, put 'exec &>/dev/null' on the first non-comment line of this script to disable ANY commands output.
+# All output is directed to the SYSLOG, to prevent this append `&>/dev/null` (without quotes) to the end of any command.
+# or, alternatively, put `exec &>/dev/null` on the first non-comment line of this script to disable ANY commands output.
 #
