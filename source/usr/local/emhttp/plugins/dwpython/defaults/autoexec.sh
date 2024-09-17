5fc7441a89f09cb0d19d0bb9b0e1e906dee57465
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-09-17 12:13:50 +0200

    Update autoexec.sh

--- source/usr/local/emhttp/plugins/dwpython/defaults/autoexec.sh
+++ source/usr/local/emhttp/plugins/dwpython/defaults/autoexec.sh
@@ -5,4 +5,5 @@
 # The purpose of this script, for example, is to establish that certain packages are always installed by means of `pip`.
 #
 # All output is directed to the SYSLOG, to prevent this append '&>/dev/null' (without quotes) to the end of any command.
+# or, alternatively, put 'exec &>/dev/null' on the first non-comment line of this script to disable ANY commands output.
 #
