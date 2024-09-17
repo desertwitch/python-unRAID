eb8e1e9a1792cae659a9716bcf8b2b829bb33e77
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-09-17 15:24:46 +0200

    Update doinst.sh

--- source/install/doinst.sh
+++ source/install/doinst.sh
@@ -17,8 +17,8 @@ cp -n $DOCROOT/default.cfg $BOOT/dwpython.cfg >/dev/null 2>&1
 cp -nr $DOCROOT/defaults/* $BOOT/scripts/ >/dev/null 2>&1
 
 # copy auto-execution script to RAM and make executable for post-install
-cp -f $BOOT/scripts/autoexec.sh /tmp/dwpython-autoexec.sh
-chmod 755 /tmp/dwpython-autoexec.sh
+cp -f $BOOT/scripts/autoexec.sh /tmp/dwpython-autoexec.sh >/dev/null 2>&1
+chmod 755 /tmp/dwpython-autoexec.sh >/dev/null 2>&1
 
 # set up plugin-specific polling tasks
 rm -f /etc/cron.daily/python-poller >/dev/null 2>&1
