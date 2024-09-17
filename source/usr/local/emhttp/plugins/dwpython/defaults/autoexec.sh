afd229d89cb9c2243d92729c7021a233dd54be05
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-09-17 15:15:30 +0200

    changes

--- source/usr/local/emhttp/plugins/dwpython/defaults/autoexec.sh
+++ source/usr/local/emhttp/plugins/dwpython/defaults/autoexec.sh
@@ -2,7 +2,9 @@
 #
 # Put here all commands to run anytime the plugin installed (e.g. at boot-time), re-installed or updated on your system.
 # They are executed NON-BLOCKING (but sequentially), ensuring boot/(re-)install proceed regardless of any script errors.
+#
 # The purpose of this script, for example, is to establish that certain global packages are always installed with `pip`.
+# Please keep in mind that using virtual environments is always preferable over globally installing any Python packages.
 #
 # All output is directed to the SYSLOG. To prevent this, append ` &>/dev/null` (without quotes) to the end of any command.
 # Alternatively, just put `exec &>/dev/null` on the first non-comment line of this script to disable ALL commands outputs.
