7356c835b0092f7c132ab21b8745e78581fce2c3
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-09-17 11:46:42 +0200

    changes

--- source/usr/local/emhttp/plugins/dwpython/defaults/autoexec.sh
+++ source/usr/local/emhttp/plugins/dwpython/defaults/autoexec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Put here all commands to run anytime the plugin installed (e.g. at boot-time), re-installed or updated. 
-# They are executed as NON-BLOCKING, to ensure that boot/install sequences proceed in case of any errors.
-# The purpose, as an example, is to establish that certain packages are always installed by means of `pip`.
-# 
+# Put here all commands to run anytime the plugin installed (e.g. at boot-time), re-installed or updated on your system.
+# They are executed NON-BLOCKING (but sequentially), ensuring boot/(re-)install proceed regardless of any script errors.
+# The purpose of this script, for example, is to establish that certain packages are always installed by means of `pip`.
+#
