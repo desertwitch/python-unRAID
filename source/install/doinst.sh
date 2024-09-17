9ecb34b699aa2c5b3989a6fcc204a5fd8110bc59
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-09-17 10:55:25 +0200

    changes

--- source/install/doinst.sh
+++ source/install/doinst.sh
@@ -17,4 +17,11 @@ cp -nr $DOCROOT/defaults/* $BOOT/scripts/ >/dev/null 2>&1
 
 # copy script files from flash drive to local system, for our plugin to use
 cp -rf $BOOT/scripts/* /etc/dwpython/ >/dev/null 2>&1
-chmod +x /etc/dwpython/*.sh >/dev/null 2>&1
+
+# Set permissions
+chmod 755 $DOCROOT/scripts/* >/dev/null 2>&1
+chmod 755 /etc/dwpython/*.sh >/dev/null 2>&1
+
+if [ "$( grep -ic "dwpython" /etc/rc.d/rc.local_shutdown )" -eq 0 ]; then
+    sed -i -e '/# Get time-out setting/i [ -x /usr/local/emhttp/plugins/dwpython/scripts/backup ] && /usr/local/emhttp/plugins/dwpython/scripts/backup' -e //N /etc/rc.d/rc.local_shutdown
+fi
