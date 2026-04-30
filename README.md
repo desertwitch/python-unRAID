dc98324aecdc80db6d3683f35394c497f4c8bae9
desertwitch <24509509+desertwitch@users.noreply.github.com>
2026-04-30 13:52:28 +0200

    changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Python 3 for UNRAID
 ================
-_Installs python3, pip and setuptools onto Unraid systems._
+_Installs python3, pip, uv and setuptools onto Unraid systems._
 
 ![GitHub Tag](https://img.shields.io/github/v/tag/desertwitch/python-unRAID?label=release&color=peru)
 [![CodeFactor](https://www.codefactor.io/repository/github/desertwitch/python-unraid/badge)](https://www.codefactor.io/repository/github/desertwitch/python-unraid)
--- source/usr/local/emhttp/plugins/dwpython/include/dwpython_config.php
+++ source/usr/local/emhttp/plugins/dwpython/include/dwpython_config.php
@@ -23,4 +23,5 @@ $dwpython_backend = trim(isset($dwpython_cfg['BACKEND']) ? htmlspecialchars($dwp
 $dwpython_py_backend = htmlspecialchars(trim(shell_exec("find /var/log/packages/ -type f -iname 'python3-*' -printf '%f\n' 2>/dev/null") ?? "-"));
 $dwpython_pip_backend = htmlspecialchars(trim(shell_exec("find /var/log/packages/ -type f -iname 'python-pip-*' -printf '%f\n' 2>/dev/null") ?? "-"));
 $dwpython_st_backend = htmlspecialchars(trim(shell_exec("find /var/log/packages/ -type f -iname 'python-setuptools-*' -printf '%f\n' 2>/dev/null") ?? "-"));
+$dwpython_uv_backend = htmlspecialchars(trim(shell_exec("find /var/log/packages/ -type f -iname 'uv-*' -printf '%f\n' 2>/dev/null") ?? "-"));
 ?>
